ef6d64fc7174ec743c732a76e0e11bde406a83b8
tkachiko <tkachikos@gmail.com>
2022-09-02 22:33:11 +0300

    01 - components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Hello() {
+  alert('Hello IT-KAMASUTRA')
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is App component
+      <Rating/>
+      <Accordeon/>
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <div>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+    </div>
+  )
+}
+
+function Star() {
+  return (
+    <div>star</div>
+  )
+}
+
+function Accordeon() {
+  return <div>
+    <h3>Menu</h3>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
 }
 
 export default App;
