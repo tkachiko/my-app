320815a862f628b27e0465b1abf8eb46f3af0986
tkachiko <tkachikos@gmail.com>
2022-09-03 17:31:33 +0300

    02 - components part 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,62 @@ function Hello() {
 }
 
 function App() {
+  console.log('App rendering')
   return (
     <div>
-      This is App component
+      <AppTitle/>
       <Rating/>
-      <Accordeon/>
+      <Accordion/>
+      <Rating/>
     </div>
   );
 }
 
+function AppTitle() {
+  console.log('AppTitle rendered')
+  return <>This is App component</>
+}
+
 function Rating() {
+  console.log('Rating rendered')
   return (
-    <div>
+    <>
       <Star/>
       <Star/>
       <Star/>
       <Star/>
       <Star/>
-    </div>
+    </>
   )
 }
 
 function Star() {
+  console.log('Star rendered')
   return (
     <div>star</div>
   )
 }
 
-function Accordeon() {
-  return <div>
-    <h3>Menu</h3>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
-  </div>
+function Accordion() {
+  console.log('Accordion rendering')
+  return <>
+    <AccordionTitle/>
+    <AccordionBody/>
+  </>
+}
+
+function AccordionTitle() {
+  console.log('AccordionTitle rendered')
+  return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+  console.log('AccordionBody rendered')
+  return <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
 }
 
 export default App;
