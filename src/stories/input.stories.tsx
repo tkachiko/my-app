39268eb76031bb23c714376ccb52deed2885b1c1
tkachiko <tkachikos@gmail.com>
2022-10-18 20:59:53 +0300

    13 - controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -7,23 +7,23 @@ export default {
 
 export const UncontrolledInput = () => <input/>;
 export const TrackValueOfUncontrolledInput = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState<string>('');
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const actualValue = e.currentTarget.value;
     setValue(actualValue);
-  }
+  };
 
   return <><input onChange={onChange}/> - {value}</>;
 };
 export const GetValueOfUncontrolledInputByButtonPress = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState<string>('');
   const inputRef = useRef<HTMLInputElement>(null);
 
   const save = () => {
     const el = inputRef.current as HTMLInputElement;
     setValue(el.value);
-  }
+  };
 
   return <><input ref={inputRef}/>
     <button onClick={save}>save
@@ -31,5 +31,28 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 
     - actual value: {value}
   </>;
+};
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState<string>('');
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value);
+
+  return <input value={parentValue} onChange={onChange}/>;
+};
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState<boolean>(true);
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.checked);
+
+  return <input type={'checkbox'} checked={parentValue} onChange={onChange}/>;
+};
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>('2');
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => setParentValue(e.currentTarget.value);
+
+  return <select onChange={onChange}>
+    <option value={parentValue}>none</option>
+    <option value={'1'}>Kiev</option>
+    <option value={'2'}>Minsk</option>
+    <option value={'3'}>Moscow</option>
+  </select>
 };
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator.ru'}/>;
