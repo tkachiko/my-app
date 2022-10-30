09ac210776875c5c0b8cd5aee001d08b9df2abdd
tkachiko <tkachikos@gmail.com>
2022-10-30 20:59:19 +0300

    15 - Custom Select component

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {Select} from './Select';
+
+export default {
+  title: 'Select',
+  component: Select,
+};
+
+export const WithValue = () => {
+  const [value, setValue] = useState('2');
+
+  return <>
+    <Select onChange={setValue}
+            value={value}
+            items={[{value: '1', title: 'Minsk'},
+              {value: '2', title: 'Moscow'},
+              {value: '3', title: 'Kiev'}]}/>
+  </>;
+};
+
+export const WithoutValue = () => {
+  const [value, setValue] = useState(null);
+
+  return <>
+    <Select onChange={setValue}
+            value={value}
+            items={[{value: '1', title: 'Minsk'},
+              {value: '2', title: 'Moscow'},
+              {value: '3', title: 'Kiev'}]}/>
+  </>;
+};
+
+
+
