890572d3bf442c2cd4abf6fc275285e7de8c5c35
tkachiko <tkachikos@gmail.com>
2022-11-13 15:18:31 +0300

    20 - useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,29 @@
+import React, {useMemo, useState} from 'react';
+
+export default {
+  title: 'useState demo',
+};
+
+function generateData() {
+  // difficult calculations
+  console.log('generateData');
+  return 323567894;
+}
+
+export const Example1 = () => {
+    console.log('Example1');
+
+    // const initValue = useMemo(generateData, []);
+
+    const [counter, setCounter] = useState<number>(generateData); // [323567894, function(newValue){}]
+
+    const changer = (state: number) => {
+      return state + 1
+    };
+
+    return <>
+      <button onClick={() => setCounter(changer)}>+</button>
+      {counter}
+    </>;
+  }
+;
